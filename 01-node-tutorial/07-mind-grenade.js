010f315d403d5ca2e4fca1df20575ab1ae4c690b
Zuhaib Amjad <umar0423@gmail.com>
2023-05-02 15:41:30 +0500

    Add lodash

--- 01-node-tutorial/07-mind-grenade.js
+++ 01-node-tutorial/07-mind-grenade.js
@@ -1,8 +1,7 @@
-const num1 = 5
-const num2 = 10
+const num1 = 10;
+const num2 = 20;
+const addValues = () => {
+  console.log(`The sum is ${num1 + num2}`);
+};
 
-function addValues() {
-  console.log(`the sum is : ${num1 + num2}`)
-}
-
-addValues()
+module.exports = addValues();
